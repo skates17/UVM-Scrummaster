b8d032ee2a2c63a4a84601ebc1a3a6a00bf7488b
Simon Anguish <simonanguish@mac.com>
2017-04-01 18:07:20 -0400

    fixed merge conflict

--- index.php
+++ index.php
@@ -17,88 +17,6 @@
 -->
 <!DOCTYPE html>
 <html>
-
-  <head>
-    
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <?php include "top.php" ?>
-    
-    <title>PigPen</title>
-    
-    <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.1/css/materialize.min.css">
-    
-    <!-- Material Icons -->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
-    <!-- Compiled and minified JavaScript -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.1/js/materialize.min.js"></script>
-    <meta name="description" content="">
-    <meta name="author" content="Jim Conallen">
-    <link rel="icon" href="./images/bluemix_icon.png">
-    
-    <meta charset="utf-8">
-    <meta name="author" content="Scrum Masters">
-    <meta name="description" content="Dor Cleaner">
-
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!--[if lt IE 9]>
-    <script src="//html5shim.googlecode.com/sin/trunk/html5.js"></script>
-    <![endif]-->
-
-    <link rel="stylesheet" href="clean.css" type="text/css" media="screen">
-    
-    
-
-    <!-- Bootstrap core CSS -->
-<!--     <link href="./css/bootstrap.min.css" rel="stylesheet"> -->
-
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <link href="./css/ie10-viewport-bug-workaround.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="starter-template.css" rel="stylesheet">
-
-    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="./js/ie-emulation-modes-warning.js"></script>
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-<body>
-    <!-- Main Nav -->
-    <nav class="light-blue lighten-4">
-      <a href="/" class="brand-logo black-text">PigPen</a>
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="?Page=registration"><i class="material-icons black-text">settings</i></a></li>
-      </ul>
-    </nav> <!-- !End nav -->
-    
-    <!-- Container for services -->
-    <div class="container">
-      <div class="row">
-        <?php
-          if (isset($_GET['Page'])) {
-            include $_GET['Page'] . ".php";
-          } else {
-            include "forum.php";
-          }
-          
-        ?>
-      </div>
-    </div> <!-- !End Container>
-=======
-=======
->>>>>>> origin/master
-<html>
 <?php include "top.php" ?>
 <body>
     <!-- Main Nav -->
@@ -122,9 +40,6 @@
         ?>
       </div>
     </div> <!-- !End Container>
->>>>>>> 6712db5a72bb46a3776f21dccb0925345379aee0
-	
-
 	
 	<!-- Bootstrap core JavaScript
     ================================================== -->
