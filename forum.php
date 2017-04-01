7add65436261b2c3e78259be45d055312b91c709
Jon Pomaika'iokalani Pornelos <jonathan.pornelos@uvm.edu>
2017-04-01 11:34:53 -0400

    Forum
    forum w/ Sql

--- forum.php
+++ forum.php
@@ -1,3 +1,24 @@
+
+<?php 
+$b="SELECT pmkPostId, fnkUsername, fldPhoto, fldPrice, fldComment,fldLocation";
+$query.=" FROM tblForum";
+$data=array($b);
+$info=$thisDatabaseReader->select($query,$data,1,0,0,0, false,false);
+
+foreach($info as $arrayRec){
+	print '<div class="forumPost">';
+	print'<div class="heading">';
+	print'<h2 class="forumH2Left">'.$arrayRec['fnkUsername'].'</h2>';
+	print'</div>';
+	print'<div class="forumPhoto"><img alt="room" src="'.$arrayRec['fldPhoto'].'" width="100">';
+	print'<h4 class = "description">Description</h4>';
+	print'<p>'.$arrayRec['fldComment'].'</p>';
+	print'<p class="price">'.$arrayRec['fldPrice'].'</p>';
+	print'<p class="Location">'.$arrayRec['fldLocation'].'</div>';
+	print'</div>';
+}
+	?>
+
 <div class="row">
 	<div class="col s12 m6">
 		<div class="forumPost card">
