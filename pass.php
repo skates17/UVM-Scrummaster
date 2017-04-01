5365bb8278c51faa8eeef2ff99c49648e4b37ee0
Alex Ram <aram1@uvm.edu>
2017-04-01 13:05:30 -0400

    database test

---
+++ pass.php
@@ -0,0 +1,7 @@
+<?php
+// passwords for your database 
+$dbReader="67zkZ6TTMrrYln9N";
+$dbWriter="fRqLlBUjK2DL4EcQ";
+$dbAdmin="Osm2BWePt886WQLf";
+
+?>
--- top.php
+++ top.php
@@ -26,7 +26,7 @@ include "lib/constants.php";
 
         print "<!-- require Database.php sdfea -->";
 
-        require_once(BIN_PATH . '/Database.php');
+        require_once(__DIR__ . '/Database.php');
 
         // %^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%
         //
