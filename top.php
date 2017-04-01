c11a3ccdc4bc0011b1e6c8679abe17d431cf68c8
Simon Anguish <simonanguish@mac.com>
2017-04-01 14:55:56 -0400

    Fixed duplicate tag in top.php

--- top.php
+++ top.php
@@ -1,87 +1,69 @@
 <?php
-include "lib/constants.php";
+    // %^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%
+    //
+    // inlcude all libraries. 
+    // %^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%
+    include "lib/constants.php";
 
-?>
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        
+    print "<!-- require Database.php sdfea -->";
 
-        <?php
-        // %^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%
-        //
-        // inlcude all libraries. 
-        // %^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%
+    require_once(__DIR__ . '/Database.php');
 
-        print "<!-- require Database.php sdfea -->";
+    // %^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%
+    //
+    // Set up database connection
+    //
+    // generally you dont need the admin on the web
 
-        require_once(__DIR__ . '/Database.php');
+    print "<!-- make Database connections -->";
+    $dbUserName = 'aram1_reader';
+    $whichPass = "r"; //flag for which one to use
+    $dbName = ARAM1_cs148;
+    
+    //print $dbUserName;
+   // print $dbName;
+    $dbName = ARAM1_cs148;
 
-        // %^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%
-        //
-        // Set up database connection
-        //
-        // generally you dont need the admin on the web
+    $thisDatabaseReader = new Database($dbUserName, $whichPass, $dbName);
 
-        print "<!-- make Database connections -->";
-        $dbUserName = 'aram1_reader';
-        $whichPass = "r"; //flag for which one to use
-        $dbName = ARAM1_cs148;
-        
-        //print $dbUserName;
-       // print $dbName;
-        $dbName = ARAM1_cs148;
+    $dbUserName = get_current_user() . '_writer';
+    $whichPass = "w";
+    $thisDatabaseWriter = new Database($dbUserName, $whichPass, $dbName);
 
-        $thisDatabaseReader = new Database($dbUserName, $whichPass, $dbName);
+    //ldap
+    function ldapName($uvmId) {
+        if (empty($uvmId))
+            return "no:netid";
 
-        $dbUserName = get_current_user() . '_writer';
-        $whichPass = "w";
-        $thisDatabaseWriter = new Database($dbUserName, $whichPass, $dbName);
+        $name = "not:found";
 
-        ?>	
-<?php 
-//ldap
-function ldapName($uvmId) {
-    if (empty($uvmId))
-        return "no:netid";
+        $ds = ldap_connect("ldap.uvm.edu");
 
-    $name = "not:found";
+        if ($ds) {
+            $r = ldap_bind($ds);
+            $dn = "uid=$uvmId,ou=People,dc=uvm,dc=edu";
+            $filter = "(|(netid=$uvmId))";
+            $findthese = array("sn", "givenname");
 
-    $ds = ldap_connect("ldap.uvm.edu");
-
-    if ($ds) {
-        $r = ldap_bind($ds);
-        $dn = "uid=$uvmId,ou=People,dc=uvm,dc=edu";
-        $filter = "(|(netid=$uvmId))";
-        $findthese = array("sn", "givenname");
-
-        // now do the search and get the results which are stored in $info
-        $sr = ldap_search($ds, $dn, $filter, $findthese);
-        
-        // if we found a match (in this example we should actually always find just one
-        if (ldap_count_entries($ds, $sr) > 0) {
-            $info = ldap_get_entries($ds, $sr);
-            $name = $info[0]["givenname"][0] . ":" . $info[0]["sn"][0];
+            // now do the search and get the results which are stored in $info
+            $sr = ldap_search($ds, $dn, $filter, $findthese);
+            
+            // if we found a match (in this example we should actually always find just one
+            if (ldap_count_entries($ds, $sr) > 0) {
+                $info = ldap_get_entries($ds, $sr);
+                $name = $info[0]["givenname"][0] . ":" . $info[0]["sn"][0];
+            }
         }
+
+        ldap_close($ds);
+
+        return $name;
     }
-
-    ldap_close($ds);
-
-    return $name;
-}
-$NetId = htmlentities($_SERVER["REMOTE_USER"], ENT_QUOTES, "UTF-8");
-$name = ldapName($NetId);
-$name = split(":", $name);
-$netIdArray = array($NetId, $name[0], $name[1]);
-$insertNames = "INSERT INTO `tblUser`(`pmkUsername`, `fldFirstName`, `fldLastName`) VALUES (?,?,?)";
-$addNames = $thisDatabaseWriter->insert($insertNames, $netIdArray, 0, 0, 0, 0, false, false);
-setcookie("netId", $NetId, time() + (60*60*24*1), '/');
+    $NetId = htmlentities($_SERVER["REMOTE_USER"], ENT_QUOTES, "UTF-8");
+    $name = ldapName($NetId);
+    $name = split(":", $name);
+    $netIdArray = array($NetId, $name[0], $name[1]);
+    $insertNames = "INSERT INTO `tblUser`(`pmkUsername`, `fldFirstName`, `fldLastName`) VALUES (?,?,?)";
+    $addNames = $thisDatabaseWriter->insert($insertNames, $netIdArray, 0, 0, 0, 0, false, false);
+    setcookie("netId", $NetId, time() + (60*60*24*1), '/');
 ?>
-    </head>
-
-    <!-- **********************     Body section      ********************** -->
-    <?php
-    print '<body id="' . $PATH_PARTS['filename'] . '">';
-    include "header.php";
-    include "nav.php";
-    ?>
