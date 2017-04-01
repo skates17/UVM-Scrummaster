c0c446f9cd709a57f57a580c9ba33cc5e83f13a9
Simon Anguish <simonanguish@mac.com>
2017-04-01 09:13:18 -0400

    Added Material CDN

--- index.php
+++ index.php
@@ -19,6 +19,11 @@
 <html>
 
   <head>
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.1/css/materialize.min.css">
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.1/js/materialize.min.js"></script>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
